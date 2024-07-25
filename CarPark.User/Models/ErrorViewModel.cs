7b7fafae5a252ac69a44b6fdc8b196bd6bd79be0
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2024-07-25 22:28:52 +0200

    Added CarPark.User

---
+++ CarPark.User/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CarPark.User.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
